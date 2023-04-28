fd0089a1c8d613dc3af83ee32621c7d4c5a3c015
levochkaa <levochkaaa@icloud.com>
2023-04-29 00:02:17 +0300

    Initial commit

---
+++ TestTDLibKit/TestTDLibKitApp.swift
@@ -0,0 +1,12 @@
+// TestTDLibKitApp.swift
+
+import SwiftUI
+
+@main
+struct TestTDLibKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
